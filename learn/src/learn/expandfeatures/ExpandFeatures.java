91d2e9436911165f3e7410e46413c6b535b562ff
Rosiane Correia Santos <pequena.padawan@gmail.com>
2013-01-21 14:13:45 -0200

    Metodos para expansao de caracteristicas

--- learn/src/learn/expandfeatures/ExpandFeatures.java
+++ learn/src/learn/expandfeatures/ExpandFeatures.java
@@ -1,6 +1,7 @@
 package learn.expandfeatures;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import learn.pojos.ClassExpression;
@@ -8,21 +9,98 @@ import learn.pojos.Predicate;
 
 public class ExpandFeatures {
 	public static List<ClassExpression> relationalExtension(
-			List<ClassExpression> seed, List<Predicate> predicates) {
+			List<ClassExpression> seed, List<Predicate> predicates)
+			throws CloneNotSupportedException {
 		List<ClassExpression> result = new ArrayList<>();
-		// TODO implementar
+		Iterator<ClassExpression> seedIterator = seed.iterator();
+		Iterator<Predicate> predicateIterator = predicates.iterator();
+		ClassExpression classExpression = null;
+		ClassExpression classExpressionNew = null;
+		Predicate predicate = null;
+		int indexArity = 0;
+		while (seedIterator.hasNext()) {
+			classExpression = seedIterator.next();
+			while (predicateIterator.hasNext()) {
+				predicate = predicateIterator.next();
+				for (indexArity = 0; indexArity < predicate.getArity(); indexArity++) {
+					classExpressionNew = new ClassExpression(new String(
+							predicate.getPredicate()), predicate.getArity());
+					classExpressionNew.setParameter(
+							indexArity,
+							new ClassExpression(new String(classExpression
+									.getPredicate()), classExpression
+									.getParameter().length));
+					result.add(classExpressionNew);
+				}
+			}
+		}
 		return result;
 	}
 
-	public static List<ClassExpression> specialize(List<ClassExpression> seed) {
+	public static List<ClassExpression> specialize(List<ClassExpression> seed,
+			List<ClassExpression> intersections) {
 		List<ClassExpression> result = new ArrayList<>();
-		// TODO implementar
+		Iterator<ClassExpression> seedIterator = seed.iterator();
+		Iterator<ClassExpression> intersectionIterator = intersections
+				.iterator();
+		ClassExpression classExpressionNew = null;
+		ClassExpression classExpression = null;
+		ClassExpression intersection = null;
+		ClassExpression parameters[] = null;
+		ClassExpression parameter = null;
+		ClassExpression intersectionNew[] = null;
+		int indexParameter = 0;
+		while (seedIterator.hasNext()) {
+			classExpression = seedIterator.next();
+			while (intersectionIterator.hasNext()) {
+				intersection = intersectionIterator.next();
+				parameters = classExpression.getParameter();
+				for (indexParameter = 0; indexParameter < parameters.length; indexParameter++) {
+					classExpressionNew = new ClassExpression(new String(
+							classExpression.getPredicate()),
+							classExpression.getParameter().length);
+					parameter = parameters[indexParameter];
+					if (parameter == null) {
+						parameter = new ClassExpression(new String(
+								intersection.getPredicate()),
+								intersection.getParameter().length);
+					} else {
+						intersectionNew = new ClassExpression[2];
+						intersectionNew[0] = new ClassExpression(new String(
+								parameter.getPredicate()),
+								parameter.getParameter().length);
+						intersectionNew[1] = new ClassExpression(new String(
+								intersection.getPredicate()),
+								intersection.getParameter().length);
+						parameter = new ClassExpression(null, 0);
+						parameter.setIntersection(intersectionNew);
+					}
+					classExpressionNew.setParameter(indexParameter, parameter);
+					result.add(classExpressionNew);
+				}
+			}
+		}
 		return result;
 	}
 
-	public static List<ClassExpression> complement(List<ClassExpression> seed) {
+	public static List<ClassExpression> complement(List<ClassExpression> seed)
+			throws CloneNotSupportedException {
 		List<ClassExpression> result = new ArrayList<>();
-		// TODO implementar
+		Iterator<ClassExpression> seedIterator = seed.iterator();
+		ClassExpression classExpressionNew = null;
+		ClassExpression classExpression = null;
+		while (seedIterator.hasNext()) {
+			classExpression = seedIterator.next();
+			classExpressionNew = new ClassExpression(new String(
+					classExpression.getPredicate()),
+					classExpression.getParameter().length);
+			if (classExpression.isNot()) {
+				classExpressionNew.setNot(false);
+			} else {
+				classExpressionNew.setNot(true);
+			}
+			result.add(classExpressionNew);
+		}
 		return result;
 	}
 
--- learn/src/learn/pojos/ClassExpression.java
+++ learn/src/learn/pojos/ClassExpression.java
@@ -2,8 +2,14 @@ package learn.pojos;
 
 public class ClassExpression {
 	private String predicate;
+	private ClassExpression[] intersection;
 	private ClassExpression[] parameter;
 	private boolean not;
+
+	public ClassExpression(String predicate, int arity) {
+		this.predicate = predicate;
+		this.parameter = new ClassExpression[arity];
+	}
 
 	public String getPredicate() {
 		return predicate;
@@ -21,11 +27,23 @@ public class ClassExpression {
 		this.parameter = parameter;
 	}
 
+	public void setParameter(int index, ClassExpression parameter) {
+		this.parameter[index] = parameter;
+	}
+
 	public boolean isNot() {
 		return not;
 	}
 
 	public void setNot(boolean not) {
 		this.not = not;
+	}
+
+	public ClassExpression[] getIntersection() {
+		return intersection;
+	}
+
+	public void setIntersection(ClassExpression[] intersection) {
+		this.intersection = intersection;
 	}
 }
